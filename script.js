50d3cf7ae1df5699efc71667bb5bc3f565d0e9a1
amithareddy12 <20r11a1229@gcet.edu.in>
2022-08-19 10:19:33 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function shopnow(){
+    alert("Directing  you to shopping site");
+}
+function addtocart(){
+    alert("you item added to cart");
+}
+function getdeal(){
+    alert("Get the best deals from Beauty Bounty");
+}
